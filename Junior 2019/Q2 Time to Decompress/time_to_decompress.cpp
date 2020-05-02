07b4b2d19d0d80cdc8988a7f21d610c483afacf9
peter-limawal <59006829+peter-limawal@users.noreply.github.com>
2020-05-02 16:21:22 -0400

    Adding time_to_decompress.cpp

---
+++ Junior 2019/Q2 Time to Decompress/time_to_decompress.cpp
@@ -0,0 +1,30 @@
+/*
+CCC Junior 2019
+Problem J2: Time to Decompress
+*/
+
+#include <iostream>
+
+int main() {
+
+  int lines, number;
+  std::string symbol, input, output = "";
+
+  std::cin >> lines;
+
+  for (int i = 0; i < lines; i++) {
+
+    std::cin >> input;
+    number = std::stoi(input);
+
+    std::cin >> symbol;
+
+    for (int k = 0; k < number; k++) {
+      output += symbol;
+    }
+    output += "\n";
+  }
+
+  std::cout << output;
+
+}

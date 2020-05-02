3d8278b4e5f0bd28d9dab8acebe83bd8c8a1cebd
peter-limawal <59006829+peter-limawal@users.noreply.github.com>
2020-05-02 18:31:23 -0400

    Adding telemarketer_or_not.cpp

---
+++ Junior 2018/Q1 Telemarketer or not/telemarketer_or_not.cpp
@@ -0,0 +1,22 @@
+/*
+CCC Junior 2019
+Problem J1: Telemarketer or not?
+*/
+
+#include <iostream>
+
+int main() {
+
+  int one, two, three, four;
+  std::cin >> one;
+  std::cin >> two;
+  std::cin >> three;
+  std::cin >> four;
+
+  if ((one == 8 || one == 9) && (four == 8 || four == 9) && (two == three)) {
+    std::cout << "ignore\n";
+  } else {
+    std::cout << "answer\n";
+  }
+
+}
